8dbec7f72cf044ad2bc64a37019d9d5d834e885b
--list <sujeet@cuvette.tech>
2025-07-15 12:42:18 +0530

    using resume link from env

--- app/data/HeroData.ts
+++ app/data/HeroData.ts
@@ -1,1 +1,1 @@
-export const resumeLink = "https://drive.google.com/file/d/1UlCZ9KQUeTsn88DEvZk4NLTVGrBgU6BH/view?usp=drive_link"
+export const resumeLink = process.env.NEXT_PUBLIC_RESUME_LINK || "";
